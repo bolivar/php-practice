301ee9cad843a6d5eb9d43de47477100adafd96e
Manuel Lema <ManuelPro@Manuels-MacBook-Pro.local>
2017-08-11 21:12:45 -0400

    connecting to a db
    printf()

---
+++ select.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+<?php
+    $db = mysqli_connect('localhost', 'root', 'root', 'nyip_edu');
+    $sql = 'SELECT * FROM page';
+    $result = mysqli_query($db, $sql);
+
+    foreach($result as $row) {
+        printf('<a href="%s">%s</a><br>',
+        htmlspecialchars($row['url']),
+        htmlspecialchars($row['url'])
+        );
+    }
+
+    mysql_close($db);
+?>
+</body>
+</html>
